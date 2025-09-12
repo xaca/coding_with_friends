9f677451dfb932b30ab0d0ff74e7f4f61612f86c
Andres Bedoya <and1res@hotmail.com>
2025-09-12 06:45:19 -0500

    update:Salida a cine

--- src/components/pages/Cine/Cine.jsx
+++ src/components/pages/Cine/Cine.jsx
@@ -1,10 +1,11 @@
 import MainLayout from "../../ui/MainLayout/MainLayout";
 
 function Cine(){
+    const form_url = "https://forms.gle/4G6ZbWYXoj3JtzzP8"
     return(<MainLayout>
        <div>
        <h1>Salida Cine</h1>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeYJ6c3OKi15_-DKJMICnWJEZtzJrS6sQ7_eN0EhUesYfATnQ/viewform?embedded=true" width="100%" height="4030" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+            <iframe src={form_url} width="100%" height="4030" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
        </div>
     </MainLayout>);
 }
