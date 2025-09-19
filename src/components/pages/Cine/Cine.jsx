f16d1fe8d4b35f99c8ee06160265a7963cfb3918
Andres Bedoya <and1res@hotmail.com>
2025-09-19 06:59:56 -0500

    update:Nueva pelicula

--- src/components/pages/Cine/Cine.jsx
+++ src/components/pages/Cine/Cine.jsx
@@ -1,11 +1,11 @@
 import MainLayout from "../../ui/MainLayout/MainLayout";
 
 function Cine(){
-    const form_url = "https://forms.gle/4G6ZbWYXoj3JtzzP8"
+    const form_url = "https://docs.google.com/forms/d/e/1FAIpQLScoZFvTAnwOVg4hW4zruOPz7KxvZJ0ZOF5n3-lcBiIJ6sBHng/viewform?usp=header"
     return(<MainLayout>
        <div>
        <h1>Salida Cine</h1>
-            <iframe src={form_url} width="100%" height="4030" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+            <iframe src={form_url} width="80%" height="4030" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
        </div>
     </MainLayout>);
 }
